2d4422ed16d0447ff59202f49c15739c5e18d73b
Viniis@132 <vinicius.dsc95@gmail.com>
2021-07-11 15:14:30 -0300

    implementado sticky no header

--- js/script.js
+++ js/script.js
@@ -21,9 +21,8 @@ document.addEventListener( 'DOMContentLoaded', function () {
   window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
 
-    header.classList.toggle("sticky", window.scrollY > 0)
+    header.classList.toggle("sticky", window.scrollY > 0) 
 
-    console.log(header);
 
   });
 } );
