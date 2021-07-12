446764d25ddb6aab2962e503b51ea86677c59486
Viniis@132 <vinicius.dsc95@gmail.com>
2021-07-12 18:01:15 -0300

    ajustes no header e banner

--- js/script.js
+++ js/script.js
@@ -18,13 +18,11 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
 
 
-  window.addEventListener("scroll",function(){
-    var header = document.querySelector("header");
+  // window.addEventListener("scroll",function(){
+  //   var header = document.querySelector("header");
+  //   header.classList.toggle("sticky", window.scrollY > 0);
+  // });
 
-    header.classList.toggle("sticky", window.scrollY > 0) 
-
-
-  });
 } );
 
 
