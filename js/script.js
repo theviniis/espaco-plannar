beb3a448dd62ce9149a183c1e65de5ec7b1eeb5d
Viniis@132 <vinicius.dsc95@gmail.com>
2021-07-14 14:58:12 -0300

    backup antes de implementação do swiperjs

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 
   new Splide( '.banner', {
-    easing: 'ease-in-out',
-    type: 'loop',
+    // type: 'slide',
     pagination: false,
+    clones:false,
     classes: {
       arrows: 'splide__arrows custom-arrows',
     }
+
   } ).mount();
 
 
   new Splide( '.depoimentos', {
     easing: 'ease-in-out',
-    // fixedWidth: '800px',
     focus: 'center',
     gap: '15ch',
     pagination: false,
